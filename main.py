577d4ac41358d6118005211daa64981b7c4b06b2
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-08 14:44:10 +0200

    added first coding activity

---
+++ main.py
@@ -0,0 +1,13 @@
+# FIRST CODING ACTIVITY
+#define variable called sold and set it to any boolean value
+sold = False#define a variable named product_name and set it to any string
+product_name = "Coke"
+
+#define a variable named quantity and set it to any integer
+quantity = 10
+
+#define a variable called price and set it to any float
+price = 23.90
+
+#define variable called sold and set it to any boolean value
+sold = False
