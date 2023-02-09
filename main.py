24a4c280463478e7b25f298b61d0d8370cf1accf
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-09 07:31:38 +0200

    ACTIVITY 3

--- main.py
+++ main.py
@@ -30,3 +30,51 @@ c = a / b
 d = a ** b
 e = a % b
 f = a * b
+
+#ACTIVITY 3
+#
+# ## YOUR CODE STARTS HERE. Replace None with the correct formula!
+#
+# ## The distance from LA to NYC in miles
+# distance_miles = 2806
+# ## Distance in Km. 1 mile = 1.609 km
+# distance_km = None
+#
+#
+# ## Temperature in degreese Celsius
+# temperature_c = 25
+# ## The temperature T in degrees Fahrenheit (°F) is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32
+# ## Temperature in Fahrenheit
+# temperature_f = None
+#
+# ## Size in inch
+# inch = 17
+# ## 1 inch is equal to 2.54 centimeters
+# ## Size in cm
+# cm = None
+#
+# ## 1 Year has 365 days, 1 day has 24h, 1h has 3600 sec
+# year_in_seconds = None
+
+## YOUR CODE STARTS HERE. Replace None with the correct formula!
+
+## The distance from LA to NYC in miles
+distance_miles = 2806
+## Distance in Km. 1 mile = 1.609 km
+distance_km = distance_miles * 1.609
+
+
+## Temperature in degreese Celsius
+temperature_c = 25
+## The temperature T in degrees Fahrenheit (°F) is equal to the temperature T in degrees Celsius (°C) times 9/5 plus 32
+## Temperature in Fahrenheit
+temperature_f = 25 * (9 / 5) + 32
+
+## Size in inch
+inch = 17
+## 1 inch is equal to 2.54 centimeters
+## Size in cm
+cm = inch * 2.54
+
+## 1 Year has 365 days, 1 day has 24h, 1h has 3600 sec
+year_in_seconds = 365 * 24 * 3600
