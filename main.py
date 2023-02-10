0ec442c127f52f871956167d1ead50c9e9139956
SeabelaSHEDO <shedo.seabela@sambeconsulting.com>
2023-02-10 08:53:15 +0200

    Strings_Challenges+ Converting types

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ info1 = "'Jonathan' is an apple variety and \"Chardonnay\" is a grape variety"
 
 # create a variable info2 that stores the string: \n is a newline escape sequence
 info2 = '\\n is a newline escape sequence'
+
+
+# Converting types
+a = '1.5'
+b = '2'
+# Using Type Casting create a new variable called c
+# that stores the result of a multiplied by b. c stores a float and will be 3.0.
+c = float(a) * float(b)
+print(c)
